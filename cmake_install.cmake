7068c73e4743579be49a9247e9ff3df5ed69ed3f
xiao <57004856+Demo-xiaobai@users.noreply.github.com>
2024-09-17 20:56:06 +0800

    Add files via upload

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/atk/linux/nerual/ncnn/benchmark
+# Install script for directory: /home/atk/linux/nerual/ncnn/examples
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
